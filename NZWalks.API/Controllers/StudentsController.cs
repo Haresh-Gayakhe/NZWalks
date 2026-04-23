ed01430f9c3516dc5739f8187bcab14462b2b480
Haresh Gayakhe <hgayakhe3@outlook.com>
2026-04-23 19:51:04 +0530

    Created students controller and added get method for testing

---
+++ NZWalks.API/Controllers/StudentsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NZWalks.API.Controllers
+{
+    // https://localhost:portnumber/api/Students
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StudentsController : ControllerBase
+    {
+        // GET: https://localhost:portnumber/api/Students
+        [HttpGet]
+        public IActionResult GetAllStudents()
+        {
+            string[] studentNames = new string[] { "Haresh", "Rohit", "Swapnil", "Shruti", "Shrutika", "Tejal" };
+
+            return Ok(studentNames);
+        }
+    }
+}
